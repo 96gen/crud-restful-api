a40d834607e83247e308e7498486902f9e105f74
96gen <96genJQ@gmail.com>
2023-04-29 20:23:57 +0800

    Configure MySQL Database for Spring Boot

---
+++ src/main/java/idv/gen96/crudrestfulapi/CrudRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package idv.gen96.crudrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idv/gen96/crudrestfulapi/CrudRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.gen96.crudrestfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
